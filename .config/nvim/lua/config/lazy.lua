e196335991507d8a8e970989e31f4e7880bf3b4c
Markus Petzke <markuspetzke@gmail.com>
2024-08-30 16:09:33 +0200

    Add files via upload

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+  -- local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  -- local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  -- if vim.v.shell_error ~= 0 then
+  --   vim.api.nvim_echo({
+  --     { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+  --     { out, "WarningMsg" },
+  --     { "\nPress any key to exit..." },
+  --   }, true, {})
+  --   vim.fn.getchar()
+  --   os.exit(1)
+  -- end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,6 +18,11 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
