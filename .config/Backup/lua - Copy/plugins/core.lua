b9c1e1e768430fe461d1e72a376c130f1da6fc55
Markus <markuspetzke@gmail.com>
2025-05-22 19:09:34 +0200

    removed old backup

--- .config/Backup/lua - Copy/plugins/core.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "folke/lazy.nvim", version = false },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-}
--- .config/Backup/lua - Copy/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-
-  { "ellisonleao/gruvbox.nvim", enabled = false },
-  { "folke/tokyonight.nvim", enabled = false },
-  { "MagicDuck/grug-far.nvim", enabled = false },
-  { "folke/persistence.nvim", enabled = false },
-}
